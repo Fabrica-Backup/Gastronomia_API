8fbe2e5d99f076a87721067f8de8226a3b1e7d82
Murilo Oliari <murilo.oliari@opet.edu.br>
2017-09-21 15:42:20 -0300

    Changed - classificacoes

--- app_gastronomia/urls.py
+++ app_gastronomia/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/unidadesmedida/', include('unidades_medida.urls')),
     url(r'^api/ingredientes/', include('ingredientes.urls')),
-    url(r'^api/classificacoes/', include('ingredientes.urls'))
+    url(r'^api/classificacoes/', include('classificacoes.urls'))
 ]
