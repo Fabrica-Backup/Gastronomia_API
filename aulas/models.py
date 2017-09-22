91952db155b166c4f7dd47532db6061491cff5d6
Erika <erikapaiva7@gmail.com>
2017-09-22 17:43:45 -0300

    Aulas
    aulas initial commit

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Aula(models.Model):
+    id_aula = models.AutoField(primary_key = True)
+    data_aula = models.DateField(default='01 de janeiro de 2000', null = True)
+    descricao_aula = models.CharField(max_length = 200)
+    aula_agendada = models.BooleanField(default= True)
+    aula_concluida = models.BooleanField(default= True)
+    periodo_aula = models.CharField(max_length = 100, null=True)
+    numero_de_alunos_projetados_aula = models.PositiveIntegerField(validators=[MaxValueValidator(50)], null=True)
+
--- aulas/views.py
+++ aulas/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .serializers import CreateAulaSerializer, ListAulaSerializer, EditAulaSerializer
+from .models import Aula
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class CreateAula(APIView):
+    serializer_class = CreateAulaSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ListAula(APIView):
+    serializer_class = ListAulaSerializer
+
+    def get(self, request, format=None):
+        serializer = self.serializer_class(Aula.objects.all(), many=True)
+        return Response(serializer.data)
+
+
+class EditAula(APIView):
+    serializer_class = EditAulaSerializer
+
+    def get(self, request, id, format=None):
+        serializer = self.serializer_class(Aula.objects.get(id_aula=id))
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        serializer = self.serializer_class(Aula.objects.get(id_aula=id), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteAula(APIView):
+    serializer_class = ListAulaSerializer
+
+    def get(self, request, id, format=None):
+        serializer = self.serializer_class(Aula.objects.get(id_aula=id))
+        return Response(serializer.data)
+
+    def delete(self, request, id, format=None):
+        Aula.objects.get(id_aula=id).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
